25c572f1c2575d39f40af83597c1c047bdb1dbaf
Muhamad Rizki Maulana <125522608+Rizkirq@users.noreply.github.com>
2024-11-11 16:49:09 +0700

    Add files via upload

---
+++ phpmvc/part11/app/views/home/index.php
@@ -0,0 +1,12 @@
+    <!-- Jumbotron -->
+    <div class="container mt-4">
+        <div class="p-5 mb-4 bg-light rounded-3">
+            <div class="container-fluid py-5">
+                <h1 class="display-5 fw-bold">Selamat Datang di Website Saya!</h1>
+                <p class="col-md-8 fs-4">Halo, nama saya <?= $data['nama']; ?></p>
+                <hr class="my-4">
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            </div>
+        </div>
+    </div>
