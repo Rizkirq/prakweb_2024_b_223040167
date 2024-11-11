81f48872e2d2d4b092bb04fcd6c444564b47d6bb
Muhamad Rizki Maulana <125522608+Rizkirq@users.noreply.github.com>
2024-11-11 23:24:19 +0700

    Add files via upload

---
+++ phpmvc/part13/app/views/about/index.php
@@ -0,0 +1,7 @@
+
+    <!-- About Me Section -->
+    <div class="container text-center">
+        <h1 class="mt-4">About me</h1>
+        <img src="<?= BASEURL; ?>/img/img.jpg" alt="iki maulana" width="200" class="rounded-circle shadow">
+        <p>Halo, nama saya <?= $data['nama']; ?> , umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+    </div>
